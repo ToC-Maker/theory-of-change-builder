0f868fb61f50bfd0aaa8a6c8a3ffa6a1344ba924
Cian Dally <cpdally3@gmail.com>
2025-08-25 10:48:54 +0100

    Updated node details in CE ToC JSON

--- src/stories/ToC.stories.ts
+++ src/stories/ToC.stories.ts
@@ -256,7 +256,7 @@ export const CharityEntrepreneurship: Story = {
           "nodes": [{
             "id": "research",
             "title": "Extensive research into promising ideas for new charities",
-            "text": "Assumptions:\n• Researcher skills, time and available information are sufficient to make recommendations worth following (Mid confidence)\n• We can continue to find promising new ideas over time i.e. the pool of shovel-ready ideas is not exhausted (High confidence)\n\nEvidence:\n• Corroboration of several of our recommendations by GiveWell and OpenPhilanthropy\n• Strong track record of CE's incubated charities (which isn't diminishing over time)",
+            "text": "Find out more about our extensive research into promising charity ideas at https://www.charityentrepreneurship.com/",
             "connectionIds": ["reports"],
 
             "connections": [{
@@ -270,7 +270,7 @@ export const CharityEntrepreneurship: Story = {
           }, {
             "id": "outreach",
             "title": "Outreach to encourage talented individuals to apply to the program",
-            "text": "Assumptions:\n• At least ~20 of the ~3000 applicants we receive per year are a good fit for charity entrepreneurship (High confidence)\n• We can continue to find promising new applicants over time, i.e. the talent pool is not exhausted (Mid confidence)",
+            "text": "Find out more about our outreach and application process at https://www.charityentrepreneurship.com/",
             "connectionIds": ["cohorts"],
 
             "connections": [{
@@ -284,7 +284,7 @@ export const CharityEntrepreneurship: Story = {
           }, {
             "id": "vetting",
             "title": "Rigorous vetting to identify the most promising applicants",
-            "text": "Assumptions:\n• Our vetting process accurately identifies the most suitable applicants for charity entrepreneurship (High confidence)\n• Selected co-founders wouldn't have had a greater impact otherwise (High confidence)\n\nEvidence:\n• Our scores of applicants during the vetting process are 0.7 correlated with internal estimates of charity impact",
+            "text": "Find out more about our rigorous vetting process at https://www.charityentrepreneurship.com/",
             "connectionIds": ["cohorts"],
 
             "connections": [{
@@ -298,7 +298,7 @@ export const CharityEntrepreneurship: Story = {
           }, {
             "id": "training",
             "title": "Improve and facilitate training program to launch an effective charity",
-            "text": "Assumptions:\n• Our new pace of running two Incubation Programs per year, of equal or higher quality, is sustainable, even as we run new types of programs (e.g. The Foundation Program) (High confidence)",
+            "text": "Find out more about our training programs for charity founders at https://www.charityentrepreneurship.com/",
             "connectionIds": ["programs"],
 
             "connections": [{
@@ -312,7 +312,7 @@ export const CharityEntrepreneurship: Story = {
           }, {
             "id": "funder-outreach",
             "title": "Outreach to intelligent, value aligned funders to join seed network",
-            "text": "Assumptions:\n• The funding landscape can support ~10 new charities per year across a range of cause areas, even in economic downturns (Mid confidence)\n• CE's reputation is strong enough that sufficient funders with good judgement want to join the network (High confidence)\n\nEvidence:\n• 83% of applications funded in last 3 programs (94% of applications to found CE recommended charity ideas)\n• Average funding: $120k",
+            "text": "Find out more about our funder outreach and seed network at https://www.charityentrepreneurship.com/",
             "connectionIds": ["seed-network"],
 
             "connections": [{
@@ -332,7 +332,7 @@ export const CharityEntrepreneurship: Story = {
           "nodes": [{
             "id": "reports",
             "title": "Reports recommending excellent ideas for new charities to launch",
-            "text": "Assumptions:\n• Recommended ideas are diverse enough for founders with different preferences to find one they're excited to launch (Mid confidence)",
+            "text": "Find out more about our charity recommendation reports at https://www.charityentrepreneurship.com/",
             "connectionIds": ["plans"],
 
             "connections": [{
@@ -346,7 +346,7 @@ export const CharityEntrepreneurship: Story = {
           }, {
             "id": "cohorts",
             "title": "Cohorts of talented participants are a good fit for entrepreneurship",
-            "text": "Assumptions:\n• Facilitation leads to strong combinations of co-founders & ideas (Low confidence)\n• Teaching equips participants with the knowledge & support they need to make smart launch plans and succeed in the field (Mid confidence)\n\nEvidence:\n• 62% of participants founded after the last 3 programs",
+            "text": "Find out more about our entrepreneurship cohorts at https://www.charityentrepreneurship.com/",
             "connectionIds": ["plans"],
 
             "connections": [{
@@ -360,7 +360,7 @@ export const CharityEntrepreneurship: Story = {
           }, {
             "id": "programs",
             "title": "Programs occur multiple times a year",
-            "text": "Assumptions:\n• Teaching equips participants with the knowledge & support they need to make smart launch plans and succeed in the field (Mid confidence)\n\nEvidence:\n• 62% of participants founded after the last 3 programs",
+            "text": "Find out more about our year-round programs at https://www.charityentrepreneurship.com/",
             "connectionIds": ["plans"],
 
             "connections": [{
@@ -375,7 +375,7 @@ export const CharityEntrepreneurship: Story = {
           }, {
             "id": "seed-network",
             "title": "Seed network with the resources and good judgement to fund deserving proposals",
-            "text": "Evidence:\n• 83% of applications funded in last 3 programs (94% of applications to found CE recommended charity ideas)\n• Average funding: $120k",
+            "text": "Find out more about our seed funding network at https://www.charityentrepreneurship.com/",
             "connectionIds": ["new-charities"],
 
             "connections": [{
@@ -395,7 +395,7 @@ export const CharityEntrepreneurship: Story = {
           "nodes": [{
             "id": "plans",
             "title": "Incubatees form co-founder teams & submit high quality plans to the seed network for funding",
-            "text": "Assumptions:\n• The seed network only funds co-founder teams with high expected counterfactual impact (High confidence)\n• Funded co-founder teams follow through on launching a charity (High confidence)",
+            "text": "Find out more about our co-founder matching and funding process at https://www.charityentrepreneurship.com/",
             "connectionIds": ["new-charities"],
 
             "connections": [{
@@ -411,7 +411,7 @@ export const CharityEntrepreneurship: Story = {
           "nodes": [{
             "id": "new-charities",
             "title": "New effective charities exist, some of which wouldn't have otherwise",
-            "text": "Assumptions:\n• Charities can get funded through the 'valley of death' (Mid confidence)\n• Organizations and co-founders don't succumb to value drift (Mid confidence)",
+            "text": "Find out more about the new effective charities we've launched at https://www.charityentrepreneurship.com/",
             "connectionIds": ["impactful-programs"],
 
             "connections": [{
@@ -427,7 +427,7 @@ export const CharityEntrepreneurship: Story = {
           "nodes": [{
             "id": "impactful-programs",
             "title": "Charities execute counterfactually impactful programs",
-            "text": "Evidence:\n• We believe ~40% of our charities are field leading, based on:\n  (a) Our internal assessments of their cost-effectiveness\n  (b) Their own public M&E results\n  (c) The endorsement of savvy funders, e.g. GiveWell → Fortify Health; OpenPhilanthropy → 8 CE charities; Founder's Pledge → FEM, LEEP & Suvita; Mulago → Suvita\n  (d) 11 external evaluations of LEEP, FEM, FWI & Suvita, by orgs like Rethink Priorities and Animal Charity Evaluators (11/11 are positive, but only a few have been made public)\n\n• External evaluations are planned for 2024.",
+            "text": "Find out more about our charities' impactful programs at https://www.charityentrepreneurship.com/",
             "connectionIds": ["wellbeing"],
 
             "connections": [{
@@ -447,7 +447,7 @@ export const CharityEntrepreneurship: Story = {
           "nodes": [{
             "id": "wellbeing",
             "title": "Improved well being for humans and animals",
-            "text": "",
+            "text": "Find out more about improving wellbeing for humans and animals at https://www.charityentrepreneurship.com/",
             "connectionIds": [],
             "connections": [],
             "yPosition": 419.25
